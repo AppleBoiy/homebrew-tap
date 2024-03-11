d6e137faa8ccb3a797e068b12f23576c44b3e5b6
Chaipat J. <work.chaipat@gmail.com>
2024-03-12 04:11:21 +0700

    update tar .sh

--- helloworld.sh.rb
+++ helloworld.sh.rb
@@ -2,7 +2,7 @@
 class HelloworldSh < Formula
   desc "a hello world program written in Bash"
   homepage "https://github.com/AppleBoiy/helloworld.js"
-  url "https://github.com/AppleBoiy/helloworld.js/releases/download/v1.0.0/helloworld.tar.gz"
+  url "https://github.com/AppleBoiy/helloworld.js/releases/download/v1.0.0/main.sh.tar.gz"
   sha256 "4d309a405ef8ef2c70049fd1c18d3bc231fd75ad33ba4ef2f0c27a088ac06df2"
   license "MIT"
 
