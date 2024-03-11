997851df588072cb2cb28ad4935d012f896cc138
Chaipat J. <work.chaipat@gmail.com>
2024-03-12 04:07:25 +0700

    update shasum helloworld.sh

--- helloworld.sh.rb
+++ helloworld.sh.rb
@@ -3,11 +3,11 @@ class HelloworldSh < Formula
   desc "a hello world program written in Bash"
   homepage "https://github.com/AppleBoiy/helloworld.js"
   url "https://github.com/AppleBoiy/helloworld.js/releases/download/v1.0.0/helloworld.tar.gz"
-  sha256 "94c1bba3dfba3926e46b8d911f4a81e1a5540df375d2dba2ed2dd6b3e76516d1"
+  sha256 "4d309a405ef8ef2c70049fd1c18d3bc231fd75ad33ba4ef2f0c27a088ac06df2"
   license "MIT"
 
   def install
-    bin.install "helloworld.sh"  
+    bin.install "helloworld`:`"
   end
 
   test do
