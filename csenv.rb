8a343854b47a03e94b4aa15e123f5b9c04aac811
Chaipat J. <work.chaipat@gmail.com>
2024-06-17 21:53:21 +0700

    chore ~

--- csenv.rb
+++ csenv.rb
@@ -2,7 +2,7 @@ class Csenv < Formula
     desc "Single command to install dependencies for courses in CSCMU."
     homepage "https://github.com/AppleBoiy/csenv"
     url "https://github.com/AppleBoiy/csenv/releases/download/0.0.1/csenv_0.0.1.tar.gz"
-    sha256 "1cc7f4aff511b6b9ca728d470842c4c71be2b55b6fe4f142831750742884b1da"
+    sha256 "989e96daccc68d931284fa55412d850adb0f7e8d8bf82967aef8f564c2576989"
     license "MIT"
   
     def install
